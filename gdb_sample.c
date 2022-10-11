978dfdca92b708d57355ced8aa884e214c4d1c1f
Satoshi OKUI <okuisatoshi@mac.com>
2022-10-11 17:43:04 +0900

    追加

---
+++ gdb_sample.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+
+char *s = "Hello world";
+//char s[16] = "Hello world";
+//char s[16] = "Hello";
+
+int main()
+{
+    int i = 0;
+    while (s[i] != ' ') {
+        i++;
+    }
+    s[i] = '\n'; s[i+1] = '\0';
+    printf("%s", s);
+}
